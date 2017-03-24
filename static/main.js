d589deea4458ea70d2be8a9da69382c970f01814
Andrew Nelson <andy@andyhub.com>
2017-03-24 00:38:50 -0500

    added example search input to use body query

--- static/main.js
+++ static/main.js
@@ -3,3 +3,9 @@ function resize() {
   var newheight = document.getElementById("widget").contentWindow.document.body.scrollHeight;
   document.getElementById("widget").height = newheight + "px";
 }
+
+function search() {
+    var url = "/geeks/civicissues/widget?labels=help wanted&number=12&body=";
+    var query = document.getElementById('project-search-bar').value;
+    document.getElementById('widget').src = url + query;
+}
